b65acd89afa19f719b8129882422108ff55ea863
Данил Высоченко <vysochenkodanil@gmail.com>
2024-12-03 21:38:41 +0300

    рихтовка

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
-
 def index(request):
     template = 'homepage/index.html'
-    return render(request, template)
+    context = {
+        'title': 'Welcome to the Homepage or hell)))',
+        'message': 'This is the homepage of my Django project.',
+    }
+    return render(request, template, context)
